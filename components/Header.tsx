60a20e76a4277fec41766793b1c38b3fcde572f0
Leonardo Barros <leonardobarros9931@gmail.com>
2023-03-10 05:01:37 -0300

    Adição de animações no Header usando FramerMotion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 function Header() {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+      initial={{
+        x: -500,
+        opacity: 0,
+        scale: 0.5,
+      }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      className="flex flex-row items-center">
         {/* Social Icons */}
         <SocialIcon
           url="https://linkedin.com/in/leonardobarros31"
@@ -21,7 +36,7 @@ function Header() {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
 
       <div className="flex flex-row items-center text-gray-300 cursor-pointer">
       <SocialIcon
